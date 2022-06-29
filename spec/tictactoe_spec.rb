b9a2264300ca9285831a47c6688a14ef1b36b58f
April <aprilmdawson@hotmail.com>
2022-06-29 16:20:54 +0100

    add error message for occupied position

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -12,23 +12,30 @@ describe 'tictactoe game'
         it 'turns the cell in the first row and first column to an X when given [1,1]' do
             game = TicTacToe.new
             game.player_1_turn(1,1)
-            expect(game.new_board[0][0]).to eq('X')
+            expect(game.board[0][0]).to eq('X')
         end
-    
 
-    # describe 'player 1 turn' do
         it 'turns the cell in the first row and second column to an X when given [1,2]' do
             game = TicTacToe.new
             game.player_1_turn(1,2)
-            expect(game.new_board[0][1]).to eq('X')
+            expect(game.board[0][1]).to eq('X')
         end
 
         it 'turns the cell in third row and third column to an X when given [3,3]' do
             game = TicTacToe.new
             game.player_1_turn(3,3)
-            expect(game.new_board[2][2]).to eq('X')
+            expect(game.board[2][2]).to eq('X')
         end
-end
+    end
+
+    describe 'check if cell is occupied' do
+        it 'should not allow a player to use an occupied cell' do
+            game = TicTacToe.new
+            game.player_1_turn(3,3)
+            expect {game.player_1_turn(3,3)}.to output("cell already occupied. choose another.").to_stdout
+        end
+    end
+
 
 
 
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,15 +1,19 @@
 class TicTacToe
     attr_accessor :board
-    attr_accessor :new_board
+    # attr_accessor :new_board
     
     def initialize()
         @board = [["-","-","-"],["-","-","-"],["-","-","-"]]
     end
 
     def player_1_turn(row,column)
-        @new_board = @board
-        @new_board[(row-1)][(column-1)] = "X"
-        print @new_board
+        if @board[(row-1)][(column-1)] == "-" 
+            @board[(row-1)][(column-1)] = "X"
+            print @board
+        else 
+            print 'cell already occupied. choose another.'
+        end
+        
     end
 
 end
