4931af9cff240b3e96b032ea07adf20f443268aa
April <aprilmdawson@hotmail.com>
2022-07-05 10:17:35 +0100

    add user input for player turn

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,10 +1,20 @@
 require_relative '../tictactoe.rb'
+require 'stringio'
+
 
 describe 'tictactoe game'
     describe 'tictactoe board' do
         it 'shows a 3x3 grid' do
             game = TicTacToe.new
             expect(game.board).to eq([['-','-','-'],['-','-','-'],['-','-','-']])
+        end
+    end
+
+    describe 'gets player 1 input' do
+        it 'returns coordinates taken from player 1' do
+            game = TicTacToe.new
+            $stdin = StringIO.new("2,2") # just automates putting user input into the terminal
+            expect(game.ask_player_position).to eq("2,2")
         end
     end
 
@@ -28,13 +38,18 @@ describe 'tictactoe game'
         end
     end
 
-    describe 'check if cell is occupied' do
-        it 'should not allow a player to use an occupied cell' do
-            game = TicTacToe.new
-            game.player_1_turn(3,3)
-            expect {game.player_1_turn(3,3)}.to output("cell already occupied. choose another.").to_stdout
-        end
-    end
+
+    
+
+    # describe 'check if cell is occupied' do
+    #     it 'should not allow a player to use an occupied cell' do
+    #         game = TicTacToe.new
+    #         game.player_1_turn(3,3)
+    #         expect {game.player_1_turn(3,3)}.to output("cell already occupied. choose another.").to_stdout
+    #     end
+    # end
+
+    
 
 
 
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,9 +1,19 @@
+require 'stringio'
+
 class TicTacToe
     attr_accessor :board
     # attr_accessor :new_board
     
     def initialize()
         @board = [["-","-","-"],["-","-","-"],["-","-","-"]]
+    end
+
+    def ask_player_position
+        puts "give us your coordinates in this format: row,column"
+        @coords_given =  $stdin.gets.chomp
+
+        #player_1_turn(coords_given)
+        # convert from string to integers
     end
 
     def player_1_turn(row,column)
